c8f208dd5bed993471d35bd1be385be4fba78f51
Mark Waltz <mwaltz@gmail.com>
2018-12-01 17:12:16 -0600

    refactor: Enhance user interface messaging

--- bikeshare.py
+++ bikeshare.py
@@ -295,7 +295,7 @@ def main():
         user_stats(df, city)
         show_raw_data(df)
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
+        restart = input('\nWould you like to restart the program? Please enter yes or no.\n')
         if restart.lower() != 'yes':
             break
 
